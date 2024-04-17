43b50b230417f1f4b65d82352cefc382263bb3c3
Mairaj Mirza <160183262+mairajmirza1981@users.noreply.github.com>
2024-04-17 16:18:29 +0500

    Add files via upload

---
+++ 10_Program_1.ts
@@ -0,0 +1,9 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+// If you don’t have anything specific to write because your programs are too simple at this point, 
+// just add your name and the current date at the top of each program file. Then write one sentencedescribing what the program does.
+
+//Exercise number 10
+// My Name is MAIRAJ MIRZA
+//Current Date 01/03/2024
+//This program will run is my Favorite mobile Number
+console.log ("3337311106");
---
+++ 10_Program_2.ts
@@ -0,0 +1,8 @@
+//Exercise number 10
+//My Name is MAIRAJ MIRZA
+//Current Date 01/03/2024
+//This program will run is Stripping Names
+console.log("\n\t\MAIRAJ MIRZA\t\n");
+
+
+

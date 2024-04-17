e2f4a90c6c455894e7cab23b136b52bce4804678
Mairaj Mirza <160183262+mairajmirza1981@users.noreply.github.com>
2024-04-17 16:16:24 +0500

    Add files via upload

---
+++ 05_Famous_Quote_2.ts
@@ -0,0 +1,8 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+// Then compose your message and store it in a new variable called message. Print your message.
+let famous_person = "Albert Einstein";
+let message = "once said, “A person who never made a mistake never tried anything new.”";
+console.log (famous_person , message);
+let famous_person2 = "Eleanor Roosevelt";
+let message2 = "once said, “No one can make you feel inferior without your consent..”";
+console.log (famous_person2 , message2);

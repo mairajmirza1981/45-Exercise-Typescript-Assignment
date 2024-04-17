356d33a04d770275b25ebd9497dde7ecd8f983eb
Mairaj Mirza <160183262+mairajmirza1981@users.noreply.github.com>
2024-04-17 16:17:43 +0500

    Add files via upload

---
+++ 09_Favorite_Number.ts
@@ -0,0 +1,5 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+// create a message that reveals your favorite number. Print that message
+
+let FavoriteNumber: number = 3337311106;
+console.log (`My Favorite Mobile Number is ${FavoriteNumber}`);
